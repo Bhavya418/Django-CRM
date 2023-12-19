b44900aad56bc297657d51b853d7dfd1cb473f11
Bhavya418 <shahb3703@gmail.com>
2023-12-19 15:36:42 +0530

    Learnt the basics of Django

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-k@n8*09f4o)5cc3*(i0f=0#(d$wk))d)lj(%y#9-*#l2nwp^(m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
